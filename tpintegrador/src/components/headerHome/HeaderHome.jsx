2c08173ad1f96a551b4827398ff9ea08b07da1b8
marcostamburi <marcose027@hotmail.com>
2024-06-10 20:55:38 -0300

    Solucion boton cambio de tema

--- tpintegrador/src/components/headerHome/HeaderHome.jsx
+++ tpintegrador/src/components/headerHome/HeaderHome.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
 import Header from "../header/Header";
 import "./Home.css";
-import { ThemeContext } from "../context/Context";
 
 const HeaderHome = () => {
+
 
   return (
     <>
@@ -14,18 +13,18 @@ const HeaderHome = () => {
           width="16"
           height="16"
           fill="currentColor"
-          className="bi bi-search"
+          className="bi bi-search zIndex5"
           viewBox="0 0 16 16"
         >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
         <input
           type="search"
-          className="input-busqueda"
+          className="input-busqueda zIndex5"
           placeholder="Buscar guarderia"
         />
-        <button className="boton-añade-guarderia">Añade tu guarderia</button>
-        <button className="iniciar-sesion">Iniciar sesión</button>
+        <button className="boton-añade-guarderia zIndex5">Añade tu guarderia</button>
+        <button className="iniciar-sesion zIndex5">Iniciar sesión</button>
         
       </div>
       
