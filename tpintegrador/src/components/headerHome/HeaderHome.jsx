8e4a8eda96df4501019f6b5b530696760ed94189
facundolgomez <facundolgomez87@gmail.com>
2024-07-25 20:11:03 -0300

    Estilizamos el home

--- tpintegrador/src/components/headerHome/HeaderHome.jsx
+++ tpintegrador/src/components/headerHome/HeaderHome.jsx
@@ -26,28 +26,10 @@ const HeaderHome = () => {
     return email.split("@")[0];
   };
 
-
-
   return (
     <>
       <Header />
       <div className={`lupa-input ${theme}`}>
-      
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-search zIndex5"
-          viewBox="0 0 16 16"
-        >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
-        <input
-          type="search"
-          className="input-busqueda zIndex5"
-          placeholder="Buscar guarderia"
-        />
         {loggedUser && loggedUser.role === "dueño" && (
           <button
             className={`boton-añade-guarderia zIndex5 ${theme}`}
@@ -62,10 +44,8 @@ const HeaderHome = () => {
             onClick={handleNavigateToUserHome}
           >
             Hacer una reserva
-          </button> 
+          </button>
         )}
-
-
 
         {loggedUser ? (
           <span
